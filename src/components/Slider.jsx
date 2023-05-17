f1ec701defe4f73734f93904b0f0fe4aa47b2091
monir3355 <16173103036@cse.bubt.edu.bd>
2023-05-18 03:38:20 +0600

    slider added

---
+++ src/components/Slider.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import img1 from "../assets/slider/slider-1.jpeg";
+import img2 from "../assets/slider/slider-2.jpeg";
+import img3 from "../assets/slider/slider-3.jpeg";
+
+const Slider = () => {
+  return (
+    <div className="carousel w-full h-[400px] md:h-[600px]">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={img1} className="w-full" />
+        <div className="absolute flex items-center justify-center gap-5 bg-black/60 bottom-0 left-0 right-0 top-0 text-center">
+          <div className="text-white space-y-7 w-5/6">
+            <h2 className="text-4xl md:text-6xl font-semibold">
+              Toys Mini Cars Bathtub Toy, Blue/White, Standard
+            </h2>
+            <p className="">
+              Melissa & Doug K's Kids Pull-Back Vehicle Set - Soft Baby Toy Set
+              With 4 Cars and Trucks and Carrying Case - Pull Back Cars, Soft
+              Vehicles Toys For Babies And Toddlers
+            </p>
+            <div>
+              <button className="btn btn-primary">All Toys</button>
+              <button className="btn btn-primary ml-4">Latest Toys</button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={img2} className="w-full" />
+        <div className="absolute flex items-center justify-center gap-5 bg-black/60 bottom-0 left-0 right-0 top-0 text-center">
+          <div className="text-white space-y-7 w-5/6">
+            <h2 className="text-4xl md:text-6xl font-semibold">
+              Toys Mini Cars Bathtub Toy, Blue/White, Standard
+            </h2>
+            <p className="">
+              Melissa & Doug K's Kids Pull-Back Vehicle Set - Soft Baby Toy Set
+              With 4 Cars and Trucks and Carrying Case - Pull Back Cars, Soft
+              Vehicles Toys For Babies And Toddlers
+            </p>
+            <div>
+              <button className="btn btn-primary">All Toys</button>
+              <button className="btn btn-primary ml-4">Latest Toys</button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={img3} className="w-full" />
+        <div className="absolute flex items-center justify-center gap-5 bg-black/60 bottom-0 left-0 right-0 top-0 text-center">
+          <div className="text-white space-y-7 w-5/6">
+            <h2 className="text-4xl md:text-6xl font-semibold">
+              Toys Mini Cars Bathtub Toy, Blue/White, Standard
+            </h2>
+            <p className="">
+              Melissa & Doug K's Kids Pull-Back Vehicle Set - Soft Baby Toy Set
+              With 4 Cars and Trucks and Carrying Case - Pull Back Cars, Soft
+              Vehicles Toys For Babies And Toddlers
+            </p>
+            <div>
+              <button className="btn btn-primary">All Toys</button>
+              <button className="btn btn-primary ml-4">Latest Toys</button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;

e9cfb3ffff6b164cf02b76e0627005377fb72a99
monir3355 <16173103036@cse.bubt.edu.bd>
2023-05-17 23:36:59 +0600

    ErrorPage added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <div>
       <Header></Header>
-      <div>
+      <div className="min-h-[calc(100vh-316px)]">
         <Outlet />
       </div>
       <Footer></Footer>
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/home/Home";
+import ErrorPage from "../components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",

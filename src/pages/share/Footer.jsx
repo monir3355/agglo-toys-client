99a98e23638c3267f6c50cf073eb38c1bce26c54
monir3355 <16173103036@cse.bubt.edu.bd>
2023-05-17 23:22:42 +0600

    headers and footer added

--- src/pages/share/Footer.jsx
+++ src/pages/share/Footer.jsx
@@ -1,21 +1,54 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer p-10 bg-base-200 text-base-content">
       <div>
-        <span className="footer-title">Services</span>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <div>
+          <Link to="/">
+            <img className="h-12" src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <p>
+          Online shopping from a great <br /> selection at Toys & Games Store.
+        </p>
       </div>
       <div>
-        <span className="footer-title">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <span className="footer-title">SERVICES</span>
+        <ul>
+          <li>
+            <Link className="link link-hover" to="/allToys">
+              All Toys
+            </Link>
+          </li>
+          <li>
+            <Link className="link link-hover" to="/myToys">
+              My Toys
+            </Link>
+          </li>
+          <li>
+            <Link className="link link-hover" to="/addToys">
+              Add Toys
+            </Link>
+          </li>
+          <li>
+            <Link className="link link-hover" to="/blogs">
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link className="link link-hover" to="/register">
+              Register
+            </Link>
+          </li>
+          <li>
+            <Link className="link link-hover" to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
       </div>
       <div>
         <span className="footer-title">Legal</span>
